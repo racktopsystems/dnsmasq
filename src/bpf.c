e72d60481955fdd95fd7d89c4e91381f76d88660
Sam Zaydel <szaydel@gmail.com>
2015-12-29 21:07:09 +0000

    Minor modifications to Makefile and src/bpf.c in order to build on OmniOS

--- src/bpf.c
+++ src/bpf.c
@@ -20,7 +20,9 @@
 #include <ifaddrs.h>
 
 #include <sys/param.h>
+#if defined(HAVE_BSD_NETWORK)
 #include <sys/sysctl.h>
+#endif
 #include <net/if.h>
 #include <net/route.h>
 #include <net/if_dl.h>
@@ -29,7 +31,7 @@
 #  include <net/if_var.h> 
 #endif
 #include <netinet/in_var.h>
-#ifdef HAVE_IPV6
+#if (defined(HAVE_IPV6) && !defined(HAVE_SOLARIS_NETWORK)) || defined(__APPLE__)
 #  include <netinet6/in6_var.h>
 #endif
 
